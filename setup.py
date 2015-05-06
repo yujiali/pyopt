46f39818ae6172525ffb87fd85d3013320170fd1
Jake Snell <jsnell@cs.toronto.edu>
2015-05-06 16:14:17 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pyopt',
+      version='0.1',
+      description='A simple python optimization package',
+      author='Yujia Li',
+      packages=['pyopt'])
